e94de4d331118d0d4c7d8cd828b727178fc8640a
Ulukyn <ulukyn@gmail.com>
2019-09-16 19:48:34 +0200

    Added: Zone lua files for 15 years event

---
+++ final_bnps/leveldesign/zones_lua/4_EF.lua
@@ -0,0 +1,3 @@
+-- Event 15 years
+SceneEditor:spawnShape("event_15years_cake.ps", 17568.688, -779.101, 9.987, 0, 0, 0.4, '{"scale y":1.05,"col size y":7.56,"col pos y":-779.101,"col size z":9.6,"scale z":1,"scale x":1.05,"col pos x":17568.688,"col orientation":2.5844421002504e+185,"col size x":8.94}')
+
