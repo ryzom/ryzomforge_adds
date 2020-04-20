4ac619ab2a6ead4f28b2ba7f8cc7a69d1d81ffa9
Ulukyn <nuno@troispetits.net>
2020-04-20 19:13:55 +0200

    Added: SE scenes from namcha

---
+++ final_bnps/leveldesign/zones_lua/45_CD.lua
@@ -0,0 +1,8 @@
+if computeCurrSeason == 4 then
+SceneEditor:spawnShape("tr_s2_palmtree_f.shape", 8952.179, -7257.768, 0.332, 0, 0.1, 0, '{"scale x":0.62,"col pos y":-7257.768,"scale y":0.62,"col pos x":8952.179,"col size z":19.267,"col orientation":0,"col size y":2.34,"col size x":2.116,"scale z":0.62}')
+SceneEditor:spawnShape("tr_s2_palmtree_b.shape", 8874.223, -7261.087, -0.395, -0.21, 0, 2.08, '{"scale x":0.72,"col pos y":-7261.087,"scale y":0.72,"col pos x":8874.223,"col size z":12.402,"col orientation":0,"col size y":7.134,"col size x":12.203,"scale z":0.72}')
+SceneEditor:spawnShape("tr_s2_palmtree_c.shape", 8871.521, -7254.671, -0.676, 0, -0.23, 0.32, '{"scale x":0.67,"col pos y":-7254.671,"scale y":0.67,"col pos x":8871.521,"col size z":16.228,"col orientation":0,"col size y":7.553,"col size x":12.203,"scale z":0.67}')
+SceneEditor:spawnShape("tr_s2_palmtree_a.shape", 8953.432, -7266.512, 0.229, 0, 0.02, 0.36, '{"scale x":0.74,"col pos y":-7266.512,"scale y":0.74,"col pos x":8953.432,"col size z":6.288,"col orientation":6.676488257602e+151,"col size y":-0.064,"col size x":0.026,"scale z":0.74}')
+SceneEditor:spawnShape("tr_s2_palmtree_a.shape", 8920.25, -7259.1, -7.06, -0.04, 0.02, 0, '{"scale x":0.74,"col pos y":-7259.1,"scale y":0.74,"col pos x":8920.25,"col size z":6.288,"col orientation":-0.216,"col size y":1.506,"col size x":1.556,"scale z":0.74}')
+end
+
