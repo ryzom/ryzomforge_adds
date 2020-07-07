d303f639b2e1cdfaf52c80988cf7844f10f03a15
Riasan <riasan@ryzom.com>
2020-07-07 15:05:20 +0200

    Added: cmdzone lua file

---
+++ final_bnps/gamedev/interfaces_v3/cmdzone.lua
@@ -0,0 +1,2 @@
+local arcc_url = "http://app.ryzom.com/app_arcc/index.php?action=scripts_RunScript&script=7800&command=reset_all"
+getUI("ui:interface:web_transactions:content:html"):browse(arcc_url)
