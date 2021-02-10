5d835a53a3d53dbd7e8512cb398747c058ceddbf
Nuno <ulukyn@gmail.com>
2021-02-10 15:08:10 +0100

    Added SE scene to kitinslair

--- final_bnps/leveldesign/zones_lua/105_AL.lua
+++ final_bnps/leveldesign/zones_lua/105_AL.lua
@@ -22,5 +22,34 @@ SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1840.976, -16837.383, -111
 -- c20d9afe0b8c8cc8e9872e4062495f7d7c6848ad
 SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1844.503, -16826.309, -113.587, 0, 0, 0, '{"col orientation":0,"col size x":17.206,"col pos y":-16826.309,"scale x":0.97,"scale y":0.97,"col pos x":1844.503,"col size z":7.989,"col size y":17.857,"scale z":0.97}')
 
+-- 106d170d8b98dbd592fd6d502ec09c5199302fc5
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1844.503, -16826.309, -113.587, 0, 0, 0, '{"col pos y":-16826.309,"col pos x":1844.503,"scale x":0.97,"col size x":17.206,"scale y":0.97,"col orientation":0,"scale z":0.97,"col size z":7.989,"col size y":17.857}')
+
+-- 5afb2dc23e8666bfcf9097772b784a93ed4d4409
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1840.976, -16837.383, -111.576, 0, 0, 0, '{"col pos y":-16837.383,"col pos x":1840.976,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":0,"scale z":1,"col size z":7.989,"col size y":17.857}')
+
+-- 32f2d26279006e5c63bbe2b1541ef759e0a80240
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1843.483, -16851.746, -109.373, 0, 0, 0, '{"col pos y":-16851.746,"col pos x":1843.483,"scale x":1.73,"col size x":17.206,"scale y":1.73,"col orientation":4.1,"scale z":1.73,"col size z":7.989,"col size y":17.857}')
+
+-- 05f23a32960ffe0867de32fc3682265f6ed983ed
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1851.853, -16813.178, -107.365, 0, 0, 0, '{"col pos y":-16813.178,"col pos x":1851.853,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":2.143,"scale z":1,"col size z":7.989,"col size y":17.857}')
+
+-- 7b7c41d06b847fb8b8f065e69d6492a0cd06e02c
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1866.561, -16822.152, -109.391, 0, 0, 0, '{"col pos y":-16822.152,"col pos x":1866.561,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":0,"scale z":1,"col size z":7.989,"col size y":17.857}')
+
+-- 54a52b87d12ee8ba4d526557687fa8db50b9e9d5
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1851.249, -16803.137, -110.377, 0, 0, 0, '{"col pos y":-16803.137,"col pos x":1851.249,"scale x":2.96,"col size x":17.206,"scale y":2.96,"col orientation":0,"scale z":2.96,"col size z":7.989,"col size y":17.857}')
+
+-- 403d5ac1b5462c65f68866f5b235279c537f1a4d
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1843.765, -16813.357, -111.437, 0, 0, 0, '{"col pos y":-16813.357,"col pos x":1843.765,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":0,"scale z":1,"col size z":7.989,"col size y":17.857}')
+
+-- cba3fc2ce9d18350bff5cef10cf9f9e8c787bdcf
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1871.488, -16811.229, -93.92, 0, -3.7, -1.47, '{"col pos y":-16811.229,"col pos x":1871.488,"scale x":1.8,"col size x":17.206,"scale y":1.8,"col orientation":0,"scale z":2.06,"col size z":7.989,"col size y":17.857}')
+
+-- 683ef93b59898ed94127aa61a0b174221e15358f
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1849.121, -16832.865, -116.298, 0, 0, 0, '{"col pos y":-16832.865,"col pos x":1849.121,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":0,"scale z":1,"col size z":7.989,"col size y":17.857}')
+
+-- 0f696208f5c33d3301802e23741e4a3e8441b523
+SceneEditor:spawnShape("ge_mission_eboulis_fy.shape", 1846.926, -16842.908, -115.202, 0, 0, 0, '{"col pos y":-16842.908,"col pos x":1846.926,"scale x":1,"col size x":17.206,"scale y":1,"col orientation":0,"scale z":1,"col size z":7.989,"col size y":17.857}')
 
 
